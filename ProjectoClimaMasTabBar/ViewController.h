31abe0a31f331e88db39c5037b5efb709e980f99
Huenei desarrollo <desarrollo@hci-mac-05.local>
2017-03-14 17:48:34 -0300

    Initial Commit

---
+++ ProjectoClimaMasTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectoClimaMasTabBar
+//
+//  Created by Huenei desarrollo on 14/3/17.
+//  Copyright © 2017 Huenei desarrollo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProjectoClimaMasTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProjectoClimaMasTabBar
+//
+//  Created by Huenei desarrollo on 14/3/17.
+//  Copyright © 2017 Huenei desarrollo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
